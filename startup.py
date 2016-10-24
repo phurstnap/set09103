36031e7c79cb185f6b2052850c1aafad1d515f65
phurstnap <firerider@live.co.uk>
2016-10-24 16:43:02 +0100

    testing link

--- startup.py
+++ startup.py
@@ -4,6 +4,22 @@ app = Flask(__name__)
 @app.route('/home/')
 def home():
 	return render_template('main.html')
+	
+@app.route('/home/gallery_people')
+def home():
+	return render_template('gallery_people.html')
+	
+@app.route('/home/gallery_animals')
+def home():
+	return render_template('gallery_animals.html')
+	
+@app.route('/home/gallery_landscape')
+def home():
+	return render_template('gallery_landscape.html')
+	
+@app.route('/home/gallery_buildings')
+def home():
+	return render_template('gallery_buildings.html')
 
 
 if __name__ == ("__main__"):
