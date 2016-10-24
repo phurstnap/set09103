d1a868d97f25f9c3f3e755bd334beb15c7e89dd2
phurstnap <firerider@live.co.uk>
2016-10-24 17:25:49 +0100

    better file names

--- startup.py
+++ startup.py
@@ -1,25 +1,25 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/home/')
-def home():
+@app.route('/gallery/')
+def gallery():
 	return render_template('gallery.html')
 	
-@app.route('/home/gallery_people')
-def gallery_people():
-	return render_template('gallery_people.html')
+@app.route('/home/people')
+def people():
+	return render_template('people.html')
 	
-@app.route('/home/gallery_animals')
-def gallery_animals():
-	return render_template('gallery_animals.html')
+@app.route('/home/animals')
+def animals():
+	return render_template('animals.html')
 	
-@app.route('/home/gallery_landscape')
-def gallery_landscape():
-	return render_template('gallery_landscape.html')
+@app.route('/home/landscape')
+def landscape():
+	return render_template('landscape.html')
 	
-@app.route('/home/gallery_buildings')
-def gallery_buildings():
-	return render_template('gallery_buildings.html')
+@app.route('/home/')
+def buildings():
+	return render_template('buildings.html')
 
 
 if __name__ == ("__main__"):
